6c0a4782f6c26de4d6824facdef98941dca02090
Karanpal00 <karan.pal.2204@gmail.com>
2026-01-06 16:45:20 +0530

    Index.html -- Added text description and div elem. style.css -- Added styling for p elem, and changed the page styling. script.js -- Added new functions(reset, addInteraction, generateGrid, getInput).

--- script.js
+++ script.js
@@ -2,18 +2,53 @@ const div = document.getElementById("container");
 const computedStyle = window.getComputedStyle(div);
 const divWidth = parseInt(computedStyle.width);
 const divHeight = parseInt(computedStyle.height);
+let divItems;
 
+function generateGrid() {
+    reset();
 
-const sizeOfGrid = prompt("Enter numer of grid between 2 to 100.");
+    const sizeOfGrid = getInput();
 
-for (let i = 0; i < sizeOfGrid*sizeOfGrid; ++i) {
-    let newDiv = document.createElement("div");
+    for (let i = 0; i < sizeOfGrid*sizeOfGrid; ++i) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("item");
 
-    newDiv.classList.add("item");
-    newDiv.style.width = (divWidth/sizeOfGrid).toString()+"px";
-    newDiv.style.height = (divHeight/sizeOfGrid).toString()+"px";
+        newDiv.style.width = (divWidth/sizeOfGrid).toString()+"px";
+        newDiv.style.height = (divHeight/sizeOfGrid).toString()+"px";
 
-    div.appendChild(newDiv);
+        div.appendChild(newDiv);
+    }
+    addInteration();
+    
+}
+
+function getInput() {
+    let sizeOfGrid = 0;
+
+    while (sizeOfGrid < 2 || sizeOfGrid > 100) {
+        sizeOfGrid = parseInt(prompt("Enter numer of grid between 2 to 100."));
+    }   
+
+    sizeOfGrid = Number.isInteger(sizeOfGrid)? sizeOfGrid: 16;
+
+    const p = document.querySelector("p");
+    p.textContent = `Color Changing Grid.
+        \nGrid size = ${sizeOfGrid}X${sizeOfGrid}`;
+
+    return sizeOfGrid;
+}
+
+function reset() {
+    while(div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+
+function addInteration() {
+    divItems = document.querySelectorAll(".item");
+    divItems.forEach((item)=> {
+        item.addEventListener("mouseenter", changeColor);
+    });
 }
 
 function changeColor(e) {
@@ -23,13 +58,13 @@ function changeColor(e) {
     e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
 }
 
-const divItems = document.querySelectorAll(".item");
+generateGrid()
 
-divItems.forEach((item)=> {
-    item.addEventListener("mouseenter", changeColor);
-});
 
-const btn = document.getElementById("btn-1");
-btn.addEventListener("click", ()=> {
+const btn1 = document.getElementById("btn-1");
+btn1.addEventListener("click", ()=> {
     divItems.forEach((item)=>item.style.backgroundColor = "transparent");
 });
+
+const btn2 = document.getElementById("btn-2");
+btn2.addEventListener("click", generateGrid);
